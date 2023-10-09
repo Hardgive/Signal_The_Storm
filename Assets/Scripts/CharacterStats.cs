36310f1f6b67aa75210dd655a51ff36c502e16fa
Петров Юрий Николаевич <hardgive@yandex.ru>
2023-10-09 19:24:23 +0300

    Initial commit

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    // ѕолучаем Id снар€жени€ персонажа дл€ экипированных слотов, чтобы потом их отобраить на модели
+    public int suitId;
+    public int headId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CharStats()
+    {
+        int suitEquipment = suitId;
+        int headEquipment = headId;
+        int health = 100;
+
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Signal_The_Storm
+ 
