a7f88495f28338358a0b5d33d8215bf665568053
Петров Юрий Николаевич <hardgive@yandex.ru>
2023-10-20 21:52:38 +0300

    Patch broken Tilemap
    Patching error in branch - broken Tilemap after merge

---
+++ Assets/Scripts/Global/Collider/Collider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        void TilemapCollider ()
+        {
+            //CharacterController character;
+
+            //Vector3 speed = character.velocity;
+        }
+    }
+}
