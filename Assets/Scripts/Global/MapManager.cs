227010ab09e7534ef4357f09d5ee54b824ac9454
NagosCrit <alexandrcrit@gmail.com>
2023-11-05 00:24:13 +0300

    per-layer tile data extraction

--- Assets/Scripts/Global/MapManager.cs
+++ Assets/Scripts/Global/MapManager.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public Grid mapCoordinateGrid;
+    [SerializeField]
+    public List<Tilemap> mapLayers;
+
+    [SerializeField]
+    private List<GridTIleData> tileData;
+
+    private Dictionary<TileBase, GridTIleData> mapTileData;
+
+    void Awake()
     {
-        
+        mapTileData = new Dictionary<TileBase, GridTIleData>();
+
+        foreach(var data in tileData)
+        {
+            foreach(var tile in data.tiles)
+            {
+                mapTileData.Add(tile, data);
+            }
+        }
+    }
+
+    private List<GridTIleData> getTileData(Vector3Int gridPos)
+    {
+        var ret = new List<GridTIleData>();
+        foreach(var tilemap in mapLayers)
+        {
+            var tile = tilemap.GetTile(gridPos);
+            if (tile is null)
+            {
+                ret.Add(null);
+                continue;
+            }
+            GridTIleData data = new GridTIleData();
+            if (mapTileData.TryGetValue(tile, out data))
+            {
+                ret.Add(data);
+            }
+            else
+            {
+                ret.Add(null);
+            }
+        }
+        return ret;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPos = mapCoordinateGrid.WorldToCell(mousePos);
+
+            var tileData = getTileData(gridPos);
+            string tileStateMsg = "";
+
+            foreach(var data in tileData)
+            {
+                if (data is null) continue;
+                tileStateMsg = tileStateMsg + 
+                    (string.Join(
+                        "; ", new List<string>(){data.name,
+                                                 data.moveResistance.ToString(),
+                                                 data.visualOpacity.ToString()}
+                        )
+                        +' '
+                        );
+            }
+            print("Clicked "+ mousePos + " tile "+ tileStateMsg);   
+
+            // GridTIleData tiledata = new GridTIleData();
+            // if(mapTileData.TryGetValue(tile, out tiledata))
+            // {
+            //     print("Clicked "+ mousePos + " tile "+ tiledata);   
+            // }
+            // else
+            // {
+            //     print("Clicked "+ mousePos + " tile not found ");   
+            // }
+            
+            
+        }
     }
 }
